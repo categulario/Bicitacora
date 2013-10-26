8162ce782b82aad513af9eda4e5af6a5bc8d99c1
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2013-10-26 02:07:42 -0500

    setup inicial de página

--- xbapp/views.py
+++ xbapp/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+def inicio(request):
+    data = {}
+    return render_to_response('xbapp/inicio.html', data, context_instance=RequestContext(request))
