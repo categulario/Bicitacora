d21d7487d9acbdd2c2617593f97d910ed4f4ac30
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2013-10-26 02:28:01 -0500

    fabfile

---
+++ fabfile.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+import os
+from fabric.api import local, settings, abort, run, env
+from fabric.contrib.console import confirm
+from fabric.context_managers import cd, lcd, settings, hide
+from fabric.decorators import task
+
+@task
+def lmigrate():
+    """Corre las migraciones respectivas"""
+    local('./dev_manage.py migrate')
