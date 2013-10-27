302ff52e2736b7c288c94ca7b63c2b428f9133cd
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2013-10-26 21:07:08 -0500

    nuevo requirement para produccion

--- xbtrack/settings.py
+++ xbtrack/settings.py
@@ -54,22 +54,22 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '/home/developingo/webapps/bicitacorastatic/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = 'http://bicitacorastatic.developingo.webfactional.com/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/developingo/webapps/bicitacorastatic/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = 'http://bicitacorastatic.developingo.webfactional.com/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
