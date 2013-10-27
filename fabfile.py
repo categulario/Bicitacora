faab1d951ad9de44341314279b97aed7f793a046
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2013-10-26 21:22:31 -0500

    fab deploy

--- fabfile.py
+++ fabfile.py
@@ -5,6 +5,18 @@ from fabric.contrib.console import confirm
 from fabric.context_managers import cd, lcd, settings, hide
 from fabric.decorators import task
 
+APP_NAME = 'XBTrack'
+USER = 'developingo'
+HOST = 'developingo.webfactional.com'
+VENV_DIR = "/home/%s/envs/EntornoBicitacora"%USER
+DJANGO_APP_ROOT = os.path.join(VENV_DIR, APP_NAME)
+GUNICORN_CONFIG = "%s/gconfig.py"%DJANGO_APP_ROOT
+GUNICORN_PIDFILE = "%s/gunicorn.pid" % DJANGO_APP_ROOT
+WSGI_MODULE = 'xbtrack.wsgi'
+STATIC_ROOT = '/home/%s/webapps/bicitacorastatic/static'%USER
+
+env.hosts = ['%s@%s' % (USER, HOST)]
+
 @task
 def lmigrate():
     """Corre las migraciones respectivas"""
@@ -14,3 +26,73 @@ def lmigrate():
 def schema():
     """Crea las migraciones"""
     local('./dev_manage.py schemamigration xbapp --auto')
+
+def virtualenv(venv_dir):
+    """
+    Context manager that establishes a virtualenv to use.
+    """
+    return settings(venv=venv_dir)
+
+def run_venv(command, **kwargs):
+    """
+    Runs a command in a virtualenv (which has been specified using
+    the virtualenv context manager)
+    """
+    run("source %s/bin/activate" % env.venv + " && " + command, **kwargs)
+
+@task
+def reqs():
+    """Instala las nuevas dependencias del paquete en el servidor remoto"""
+    with virtualenv(VENV_DIR):
+        with cd(DJANGO_APP_ROOT):
+            run_venv("pip install -r requirements.txt")
+
+@task
+def pull():
+    with cd(DJANGO_APP_ROOT):
+        run("git pull")
+
+@task
+def server_start():
+    """Inicia el servidor"""
+    run("%(venv_dir)s/bin/gunicorn -c %(config_file)s %(wsgimodule)s:application"%{
+        'venv_dir': VENV_DIR,
+        'config_file': GUNICORN_CONFIG,
+        'wsgimodule': WSGI_MODULE
+    })
+
+@task
+def server_restart():
+    """
+    Restarts the webserver that is running the Django instance
+    """
+    try:
+        run("kill -HUP $(cat %s)" % GUNICORN_PIDFILE)
+    except:
+        server_start()
+
+@task
+def server_stop():
+    try:
+        run("kill -HUP $(cat %s)" % GUNICORN_PIDFILE)
+    except:
+        print 'el servidor no está corriendo'
+
+@task
+def memory():
+    """Monitorea la memoria usada"""
+    run("ps -u %s -o pid,rss,command | awk '{print $0}{sum+=$2} END {print \"Total\", sum/1024, \"MB\"}'"%USER)
+
+@task
+def static():
+    """Recolecta los archivos estáticos en remoto"""
+    with virtualenv(VENV_DIR):
+        with cd(DJANGO_APP_ROOT):
+            run_venv("./manage.py collectstatic -v 0 --noinput --clear")
+    run("chmod -R ugo+r %s" % STATIC_ROOT)
+
+def deploy():
+    pull()
+    reqs()
+    static()
+    server_restart()
---
+++ gconfig.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+import multiprocessing
+import os
+
+bind = "127.0.0.1:25009"
+workers = 2 #multiprocessing.cpu_count() * 2 + 1
+preload_app = True #Carga el código de la aplicación antes de iniciar los trabajadores
+# Disminuye el uso de RAM pero hace más lento el reinicio del servidor
+daemon = True
+pidfile = os.path.normpath(os.path.join(os.path.dirname(__file__),'gunicorn.pid'))
+accesslog = os.path.normpath(os.path.join(os.path.dirname(__file__),'access.log'))
+errorlog = os.path.normpath(os.path.join(os.path.dirname(__file__),'error.log'))
+loglevel = 'info'
